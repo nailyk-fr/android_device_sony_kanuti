23ac0b54bdcd0f5706ee4738aec6d05a086440c2
Pavel Dubrova <pashadubrova@gmail.com>
2018-02-28 19:10:39 +0200

    Migrate to the CAF HAL's
    Now we will use CAF HAL's for Tulip device.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -8,19 +8,12 @@ MSM_VIDC_TARGET_LIST := msm8226 msm8916 msm8952 msm8974 msm8994 msm8996 msm8998 
 #List of targets that use master side content protection
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660
 
-audio-hal := hardware/qcom/audio
-gps-hal := hardware/qcom/gps/sdm845
-display-hal := hardware/qcom/display/msm8998
-QCOM_MEDIA_ROOT := hardware/qcom/media/msm8998
-OMX_VIDEO_PATH := mm-video-v4l2
-media-hal := hardware/qcom/media/msm8998
+TARGET_QCOM_AUDIO_VARIANT := caf
+TARGET_QCOM_DISPLAY_VARIANT := caf
+TARGET_QCOM_GPS_VARIANT := caf
+TARGET_QCOM_MEDIA_VARIANT := caf
 
 include device/sony/common-legacy/hardware/qcom/utils.mk
-
-include $(display-hal)/Android.mk
-include $(call all-makefiles-under,$(audio-hal))
-include $(call all-makefiles-under,$(gps-hal))
-include $(call all-makefiles-under,$(media-hal))
 
 ifeq ($(BOARD_HAVE_BLUETOOTH_QCOM),true)
   include $(call all-makefiles-under,hardware/qcom/bt/msm8998)
